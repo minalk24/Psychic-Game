159d2f4415cb02c6358d908dcede0e26f41aa718
Minal Kharche <minal.berkeley@gmail.com>
2019-05-23 19:25:39 -0700

    added conditions and displayed result

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,14 +4,20 @@ var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","
 //create a variable that hold wins, losses, starting at 0 and guesses left starting at 9
 var wins = 0;
 var losses = 0;
-var guessLeft = 9;
-var playerChoice = [];
+var guessesLeftStill = 9;
+var playersChoice = [];
 
 //create variable that hold reference to DOM element
 var winsDisplay = document.getElementById("wins");
 var lossesDisplay = document.getElementById("losses");
-var guessesLeftDisplay = document.getElementById("guessesleft");
-var playerGuessDisplay = document.getElementById("playerGuesses");
+var guessesLeftDisplay = document.getElementById("guessesLeft");
+var playerInputDisplay = document.getElementById("playerInput");
+
+//win, loss, user guess displayy
+winsDisplay.textContent = "wins: " + wins;
+lossesDisplay.textContent = "losses: " + losses;
+guessesLeftDisplay.textContent = "Guesses Left: " + guessesLeftStill;
+playerInputDisplay.textContent = "Your guesses so far: " + playersChoice;
 
 //when user press key then folling function runs
 document.onkeyup = function(event){
@@ -24,4 +30,31 @@ document.onkeyup = function(event){
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     console.log(computerGuess);
 
-}
+    if (userGuess != null){
+        playersChoice.push(userGuess);
+    }
+
+    //
+    guessesLeftDisplay.textContent = "Guesses Left" + guessesLeftStill;
+    console.log(guessesLeftStill);
+
+    //if user put uppercase value then convert into lowercase win or loss logic
+    if((userGuess.toLowerCase() === computerGuess) ) {
+        wins ++;
+        playersChoice = [];
+        guessesLeftStill = 9;
+    }else if(guessesLeftStill === 0){
+        losses ++;
+        playersChoice = [];
+        guessesLeftStill = 9;
+    }else{
+        guessesLeftStill -= 1;
+    }
+
+    //win, loss, user guess displayy
+    winsDisplay.textContent = "wins: " + wins;
+    lossesDisplay.textContent = "Losses: " + losses;
+    guessesLeftDisplay.textContent = "Guesses Left: " + guessesLeftStill;
+    playerInputDisplay.textContent = "Your guesses so far: " + playersChoice;
+
+};
